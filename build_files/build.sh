aa552479ac51cb0153451b94146557777c7a33d8
Leo Levitt <leo@leolevitt.com>
2026-05-14 12:28:09 -0400

    Trying out just using and modifying KDE/Plasma

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,117 +2,19 @@
 
 set -ouex pipefail
 
-### Remove KDE/Plasma
+### Install packages
 
-dnf5 remove -y \
-    plasma-workspace \
-    plasma-*    \
-    kde-*       \
-    kwin*       \
-    breeze*
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 autoremove -y
+# Programs I want
+dnf5 install -y					\
+		kitty
 
-### Install Packages
+# KDE customizations I want
+dnf copr enable infinality/kwin-effects-better-blur-dx
+dnf install kwin-effects-better-blur-dx
 
-dnf5 -y copr enable avengemedia/dms
-dnf5 -y copr enable avengemedia/danklinux
-dnf5 -y copr enable yalter/niri
-dnf5 -y copr enable solopasha/hyprland
-
-# Niri and DMS with dependencies
-dnf5 -y install						\
-		gdm							\
-		niri						\
-		xwayland-satellite			\
-		xdg-desktop-portal-gnome	\
-		dms							\
-		cups-pk-helper				\
-		cava						\
-		matugen						\
-		cliphist					\
-		dsearch						\
-		qt6-qtmultimedia			\
-		kf6-kimageformats
-
-# Desktop support software
-dnf5 -y install						\
-		papirus-icon-theme			\
-		breeze-cursor-theme			\
-		hyprlock					\
-		swayidle					\
-		gvfs-mtp					\
-		gvfs-gphoto2				\
-		gvfs-smb					\
-		gvfs-fuse					\
-		gvfs-archive				\
-		tumbler						\
-		file-roller
-
-# My software
-dnf5 -y install						\
-		kitty						\
-		fuzzel						\
-		nautilus					\
-		pavucontrol
-
-
-dnf5 -y copr disable avengemedia/dms
-dnf5 -y copr disable avengemedia/danklinux
-dnf5 -y copr disable yalter/niri
-dnf5 -y copr disable solopasha/hyprland
-
-# Disable terra-mesa repo: its gpgkey uses a file:// path that bootc-image-builder
-# can't resolve when building ISO/disk images. Packages are already in the image.
-sed -i 's/^enabled=1/enabled=0/' /etc/yum.repos.d/terra-mesa.repo
-
-### Configure
-
-# Remove extra desktop files
-rm -f /usr/share/wayland-sessions/plasma-steamos-wayland-oneshot.desktop
-rm -f /usr/share/wayland-sessions/gamescope-session.desktop
-rm -f /usr/share/wayland-sessions/gamescope-session-steam.desktop
-rm -f /usr/share/wayland-sessions/gnome.desktop
-rm -f /usr/share/wayland-sessions/gnome-wayland.desktop
-
-# Remove unused skel configuration
-rm -f /etc/skel/.config/kcminputrc
-
-# bootc install configuration
-install -Dm644 /dev/stdin /usr/lib/bootc/install/00-default.toml <<'EOF'
-[install]
-root-fs-type = "btrfs"
-EOF
-
-# GTK icon theme
-install -Dm644 /dev/stdin /etc/gtk-3.0/settings.ini <<'EOF'
-[Settings]
-gtk-icon-theme-name=Papirus
-gtk-cursor-theme-name=Breeze
-EOF
-
-install -Dm644 /dev/stdin /etc/gtk-4.0/settings.ini <<'EOF'
-[Settings]
-gtk-icon-theme-name=Papirus
-gtk-cursor-theme-name=Breeze
-EOF
-
-# Hyprlock default config
-install -Dm644 /ctx/hyprlock.conf /etc/xdg/hypr/hyprlock.conf
-
-# User systemd services
-install -Dm644 /ctx/ssh-agent.service /usr/lib/systemd/user/ssh-agent.service
-install -Dm644 /ctx/swayidle.service /usr/lib/systemd/user/swayidle.service
-
-# User session defaults
-install -Dm644 /ctx/ssh-agent-env.conf /etc/skel/.config/environment.d/ssh-agent.conf
-install -Dm644 /ctx/dms-settings.json /etc/skel/.config/DankMaterialShell/settings.json
-install -Dm644 /ctx/niri-config.kdl /etc/skel/.config/niri/config.kdl
-
-# System services
-systemctl enable gdm
-systemctl enable podman.socket
-
-systemctl --global add-wants niri.service dms
-systemctl --global add-wants niri.service swayidle.service
-systemctl --global add-wants niri.service ssh-agent.service
+dnf copr disable infinality/kwin-effects-better-blur-dx
